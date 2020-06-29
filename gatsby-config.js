ca41cb161156cb0449e7939f8a5eb98388728f45
Seungjune Kim <kimsj5@brandi.co.kr>
2020-06-29 21:44:22 +0900

    google analytics
    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,21 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-171114262-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content`,
@@ -211,6 +226,6 @@ module.exports = {
         sitemap: 'https://dansjkim.github.io/sitemap.xml',
         policy: [{userAgent: '*', allow: '/'}]
       }
-    }
+    },
   ]
 };
